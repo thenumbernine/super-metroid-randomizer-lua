2334dd2654fd9a5f190590a886b1381fdf534b2b
Christopher Moore <christopher.e.moore@gmail.com>
2021-09-13 00:57:00 +0600

    moved more roomstate construction into the roomstate constructor

--- room.lua
+++ room.lua
@@ -1,7 +1,6 @@
 local ffi = require 'ffi'
 local class = require 'ext.class'
 local table = require 'ext.table'
-local range = require 'ext.range'
 local frompc = require 'pc'.from
 local topc = require 'pc'.to
 local struct = require 'struct'
@@ -105,8 +104,12 @@ function Room:init(args)
 		if select_ctype == 'roomselect1_t' then break end	-- term
 	end
 
-	self.roomStates = roomSelects:mapi(function(roomSelect)
-		return RoomState{
+	-- process in reverse order so plms will be saved int ehs ame order
+	-- so that door indexes will be mainetained when writing out
+	self.roomStates = table()
+	for i=#roomSelects,1,-1 do
+		local roomSelect = roomSelects[i]
+		self.roomStates[i] = RoomState{
 			sm = sm,
 			room = self,
 			roomSelect = roomSelect,
@@ -119,7 +122,7 @@ function Room:init(args)
 				and data - rom
 				or topc(sm.roomStateBank, roomSelect:obj().roomStatePageOffset),
 		}
-	end)
+	end
 
 	-- I wonder if I can assert that all the roomstate_t's are in contiguous memory after the roomselect's ... 
 	-- they sure aren't sequential
@@ -131,151 +134,17 @@ function Room:init(args)
 	end
 	--]]
 
-	for _,rs in ipairs(self.roomStates) do
-		if rs:obj().scrollPageOffset > 0x0001 and rs:obj().scrollPageOffset ~= 0x8000 then
-			local addr = topc(sm.scrollBank, rs:obj().scrollPageOffset)
-			local size = self:obj().width * self:obj().height
-			rs.scrollData = range(size):map(function(i)
-				return rom[addr+i-1]
-			end)
-		end
-	end
-
-	-- add plms in reverse order, because the roomstates are in reverse order of roomselects,
-	-- and the plms are stored in-order with roomselects
-	-- so now, when writing them out, they will be in the same order in memory as they were when being read in
-	for i=#self.roomStates,1,-1 do
-		local rs = self.roomStates[i]
-		if rs:obj().plmPageOffset ~= 0 then
-			local plmset = sm:mapAddPLMSetFromAddr(topc(sm.plmBank, rs:obj().plmPageOffset), self)
-			rs:setPLMSet(plmset)
-		end
-	end
-
-	-- enemySpawnSet
-	-- but notice, for writing back enemy spawn sets, sometimes there's odd padding in there, like -1, 3, etc
-	for _,rs in ipairs(self.roomStates) do
-		local enemySpawnSet = sm:mapAddEnemySpawnSet(topc(sm.enemySpawnBank, rs:obj().enemySpawnPageOffset))
-		rs:setEnemySpawnSet(enemySpawnSet)
-	end
-	
-	for _,rs in ipairs(self.roomStates) do
-		rs:setEnemyGFXSet(sm:mapAddEnemyGFXSet(topc(sm.enemyGFXBank, rs:obj().enemyGFXPageOffset)))
-	end
-
-	-- some rooms use the same fx1 ptr
-	-- and from there they are read in contiguous blocks until a term is encountered
-	-- so I should make these fx1sets (like plmsets)
-	-- unless -- another optimization -- is, if one room's fx1's (or plms) are a subset of another,
-	-- then make one set and just put the subset's at the end
-	-- (unless the order matters...)
-	for _,rs in ipairs(self.roomStates) do
-		local startaddr = topc(sm.fx1Bank, rs:obj().fx1PageOffset)
+	do -- list of door offsets
+		local startaddr = topc(sm.doorAddrBank, self:obj().doorPageOffset)
 		local addr = startaddr
-		local retry
-		while true do
-			local cmd = ffi.cast('uint16_t*', rom+addr)[0]
-			
-			-- null sets are represented as an immediate ffff
-			-- whereas sets of more than 1 value use 0000 as a term ...
-			-- They can also be used to terminate a set of fx1_t
-			if cmd == 0xffff then
-				-- include terminator bytes in block length:
-				rs.fx1term = true
-				addr = addr + 2
-				break
-			end
-			
-			--if cmd == 0
-			-- TODO this condition was in smlib, but self.doors won't be complete until after all doors have been loaded
-			--or self.doors:find(nil, function(door) return door.addr == cmd end)
-			--then
-			if true then
-				local fx1 = sm:mapAddFX1(addr)
--- this misses 5 fx1_t's
-local done = fx1.ptr.doorPageOffset == 0 
-				fx1.rooms:insert(self)
-				rs.fx1s:insert(fx1)
-				
-				addr = addr + ffi.sizeof'fx1_t'
-
--- term of 0 past the first entry
-if done then break end
-			end
-		end
-	end
-
-	for _,rs in ipairs(self.roomStates) do
-		if rs:obj().bgPageOffset > 0x8000 then
-			local addr = topc(sm.bgBank, rs:obj().bgPageOffset)
-			while true do
-				local bg = sm:mapAddBG(addr, rom)
-				bg.roomStates:insert(rs)
-				rs.bgs:insert(bg)
-				addr = addr + ffi.sizeof(bg.type.name)
-				if bg.obj.header == 0 then break end
-			end
-		end
-	end
-
-	for _,rs in ipairs(self.roomStates) do
-		if rs:obj().layerHandlingPageOffset > 0x8000 then
-			local addr = topc(sm.layerHandlingBank, rs:obj().layerHandlingPageOffset)
-			rs.layerHandlingPageOffset = sm:mapAddLayerHandling(addr)
-			rs.layerHandlingPageOffset.roomStates:insert(rs)
-		end
-	
-		xpcall(function()
-			rs:setRoomBlockData(sm:mapAddRoomBlockData(rs:obj().roomBlockAddr24:topc(), self))
-		end, function(err)
-			print(err..'\n'..debug.traceback())
-		end)
-	end
-
-	-- list of door offsets
-	local startaddr = topc(sm.doorAddrBank, self:obj().doorPageOffset)
-	local addr = startaddr
-	local doorPageOffset = ffi.cast('uint16_t*', rom + addr)[0]
-	addr = addr + 2
-	-- TODO should this test be > or >= ?
-	while doorPageOffset > 0x8000 do
-		self.doors:insert(sm:mapAddDoor(topc(sm.doorBank, doorPageOffset)))
-		doorPageOffset = ffi.cast('uint16_t*', rom + addr)[0]
+		local doorPageOffset = ffi.cast('uint16_t*', rom + addr)[0]
 		addr = addr + 2
-	end
-	
-
-	-- $079804 - 00/15 - grey torizo room - has 14 bytes here 
-	-- pointed to by room[00/15].roomstate_t[#1].roomvarPageOffset
-	-- has data $986b: 0f 0a 52 00 | 0f 0b 52 00 | 0f 0c 52 00 | 00 00
-	-- this is the rescue animals roomstate
-	-- so this data has to do with the destructable wall on the right side
-	--if roomPageOffset == 0x79804 then
-	for _,rs in ipairs(self.roomStates) do
-		if rs:obj().roomvarPageOffset ~= 0 then
-			local d = rom + topc(sm.plmBank, rs:obj().roomvarPageOffset)
-			local roomvar = table()
-			repeat
-				roomvar:insert(d[0])	-- x
-				roomvar:insert(d[1])	-- y
-				if ffi.cast('uint16_t*', d)[0] == 0 then break end
-				roomvar:insert(d[2])	-- mod 1 == 0x52
-				roomvar:insert(d[3])	-- mod 2 == 0x00
-				-- TODO insert roomvar_t and uint16_t term (or omit term)
-				d = d + 4
-			until false
-			-- TODO should be roomstate
-			rs.roomvar = roomvar
+		-- TODO should this test be > or >= ?
+		while doorPageOffset > 0x8000 do
+			self.doors:insert(sm:mapAddDoor(topc(sm.doorBank, doorPageOffset)))
+			doorPageOffset = ffi.cast('uint16_t*', rom + addr)[0]
+			addr = addr + 2
 		end
-	end
-
-	-- try to load tile graphics from rs.tileSet
-	-- TODO cache this per tileSet, since there are only 256 possible, and probably much less used?
-	for _,rs in ipairs(self.roomStates) do
-		local tileSetIndex = rs:obj().tileSet
-		local tileSet = assert(sm.tileSets[tileSetIndex+1])
-		assert(tileSet.index == tileSetIndex)
-		rs:setTileSet(tileSet)
 	end
 end
 
--- roomstate.lua
+++ roomstate.lua
@@ -1,8 +1,10 @@
 local ffi = require 'ffi'
 local class = require 'ext.class'
 local table = require 'ext.table'
+local range = require 'ext.range'
 local struct = require 'struct'
 local Blob = require 'blob'
+local topc = require 'pc'.to
 
 
 local roomstate_t = struct{
@@ -65,8 +67,130 @@ function RoomState:init(args)
 	self.room = assert(args.room)
 	self.roomSelect = assert(args.roomSelect)
 	
+	local sm = self.sm
+	local rom = sm.rom
+	local m = self.room
+	
 	self.fx1s = self.fx1s or table()
 	self.bgs = self.bgs or table()
+
+	-- TODO > 0x8000 ?
+	if self:obj().scrollPageOffset > 0x0001 
+	and self:obj().scrollPageOffset ~= 0x8000 
+	then
+		local addr = topc(sm.scrollBank, self:obj().scrollPageOffset)
+		local size = m:obj().width * m:obj().height
+		self.scrollData = range(size):map(function(i)
+			return rom[addr+i-1]
+		end)
+	end
+
+	if self:obj().plmPageOffset ~= 0 then
+		self:setPLMSet(sm:mapAddPLMSetFromAddr(topc(sm.plmBank, self:obj().plmPageOffset), m))
+	end
+	
+	self:setEnemySpawnSet(sm:mapAddEnemySpawnSet(topc(sm.enemySpawnBank, self:obj().enemySpawnPageOffset)))
+	
+	self:setEnemyGFXSet(sm:mapAddEnemyGFXSet(topc(sm.enemyGFXBank, self:obj().enemyGFXPageOffset)))
+
+	-- some rooms use the same fx1 ptr
+	-- and from there they are read in contiguous blocks until a term is encountered
+	-- so I should make these fx1sets (like plmsets)
+	-- unless -- another optimization -- is, if one room's fx1's (or plms) are a subset of another,
+	-- then make one set and just put the subset's at the end
+	-- (unless the order matters...)
+	do
+		local startaddr = topc(sm.fx1Bank, self:obj().fx1PageOffset)
+		local addr = startaddr
+		local retry
+		while true do
+			local cmd = ffi.cast('uint16_t*', rom+addr)[0]
+			
+			-- null sets are represented as an immediate ffff
+			-- whereas sets of more than 1 value use 0000 as a term ...
+			-- They can also be used to terminate a set of fx1_t
+			if cmd == 0xffff then
+				-- include terminator bytes in block length:
+				self.fx1term = true
+				addr = addr + 2
+				break
+			end
+			
+			--if cmd == 0
+			-- TODO this condition was in smlib, but m.doors won't be complete until after all doors have been loaded
+			--or m.doors:find(nil, function(door) return door.addr == cmd end)
+			--then
+			if true then
+				local fx1 = sm:mapAddFX1(addr)
+-- this misses 5 fx1_t's
+local done = fx1.ptr.doorPageOffset == 0 
+				fx1.rooms:insert(m)
+				self.fx1s:insert(fx1)
+				
+				addr = addr + ffi.sizeof'fx1_t'
+
+-- term of 0 past the first entry
+if done then break end
+			end
+		end
+	end
+
+	if self:obj().bgPageOffset > 0x8000 then
+		local addr = topc(sm.bgBank, self:obj().bgPageOffset)
+		while true do
+			local bg = sm:mapAddBG(addr, rom)
+			bg.roomStates:insert(self)
+			self.bgs:insert(bg)
+			addr = addr + ffi.sizeof(bg.type.name)
+			if bg.obj.header == 0 then break end
+		end
+	end
+
+	do
+		if self:obj().layerHandlingPageOffset > 0x8000 then
+			local addr = topc(sm.layerHandlingBank, self:obj().layerHandlingPageOffset)
+			self.layerHandlingPageOffset = sm:mapAddLayerHandling(addr)
+			self.layerHandlingPageOffset.roomStates:insert(self)
+		end
+
+		xpcall(function()
+			self:setRoomBlockData(sm:mapAddRoomBlockData(self:obj().roomBlockAddr24:topc(), sm))
+		end, function(err)
+			print(err..'\n'..debug.traceback())
+		end)
+	end
+
+	
+	-- $079804 - 00/15 - grey torizo room - has 14 bytes here 
+	-- pointed to by room[00/15].roomstate_t[#1].roomvarPageOffset
+	-- has data $986b: 0f 0a 52 00 | 0f 0b 52 00 | 0f 0c 52 00 | 00 00
+	-- this is the rescue animals roomstate
+	-- so this data has to do with the destructable wall on the right side
+	--if roomPageOffset == 0x79804 then
+	if self:obj().roomvarPageOffset ~= 0 then
+		local d = rom + topc(sm.plmBank, self:obj().roomvarPageOffset)
+		local roomvar = table()
+		repeat
+			roomvar:insert(d[0])	-- x
+			roomvar:insert(d[1])	-- y
+			if ffi.cast('uint16_t*', d)[0] == 0 then break end
+			roomvar:insert(d[2])	-- mod 1 == 0x52
+			roomvar:insert(d[3])	-- mod 2 == 0x00
+			-- TODO insert roomvar_t and uint16_t term (or omit term)
+			d = d + 4
+		until false
+		-- TODO should be roomstate
+		self.roomvar = roomvar
+	end
+
+	-- try to load tile graphics from rs.tileSet
+	-- TODO cache this per tileSet, since there are only 256 possible, and probably much less used?
+	do
+		local tileSetIndex = self:obj().tileSet
+		local tileSet = assert(sm.tileSets[tileSetIndex+1])
+		assert(tileSet.index == tileSetIndex)
+		self:setTileSet(tileSet)
+	end
 end
 
 function RoomState:setPLMSet(plmset)
