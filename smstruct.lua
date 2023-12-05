d431d1d8f7d5c15dfa42c52901db043dd18dd4e3
Christopher Moore <christopher.e.moore@gmail.com>
2023-12-05 23:24:03 +0700

    new struct-lua

--- smstruct.lua
+++ smstruct.lua
@@ -1,5 +1,25 @@
+local table = require 'ext.table'
 local struct = require 'struct'
 struct.typeToString.uint8_t = struct.hextostr(2)
 struct.typeToString.uint16_t = struct.hextostr(4)
-struct.packed = true
-return struct
+return function(args)
+	return struct{
+		name = args.name,
+		union = true,
+		fields = {
+			{name='s', type='uint8_t[1]', no_iter=true},
+			{type=struct{
+				anonymous = true,
+				packed = true,
+				fields = table.mapi(args.fields, function(kv)
+					local name, ctype = next(kv)
+					return {
+						name = name,
+						type = ctype,
+					}
+				end),
+			}},
+		},
+		metatable = args.metatable,
+	}
+end
